ad2da4b6fae4a51072e058b2e697760356ea0613
Alex Lin <alin5250@gmail.com>
2018-11-18 20:51:47 -0500

    added timeplot vis

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var incidentsMap;
 // Load data
 loadData();
 
+// Date parser to convert strings to date objects
+var parseDate = d3.timeParse("%m/%d/%Y");
+
 function loadData() {
     d3.csv("data/stage3-min.csv", function(error, data){
         data.forEach(function(d) {
@@ -13,16 +16,17 @@ function loadData() {
             d.n_injured = +d.n_injured;
             d.n_killed = +d.n_killed;
             d.casualties =  d.n_killed;
+            d.date = parseDate(d.date);
         });
-        allData = data;
-        createVis();
+        createVis(data);
     });
 }
 
-function createVis() {
+function createVis(data) {
 
     // TO-DO: Instantiate visualization objects here
 
-    incidentsMap = new IncidentsMap("incidents-map", allData)
+    incidentsMap = new IncidentsMap("incidents-map", data);
+    timeplot = new TimePlot("timeplot", data);
 
 }
---
+++ js/timeplot.js
@@ -0,0 +1,208 @@
+/*
+ * Timeplot - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data				-- The entire gun violence dataset
+ */
+
+TimePlot = function(_parentElement, _data){
+    this.parentElement = _parentElement;
+    this.allData = _data;
+    this.displayData = []; // see data wrangling
+
+    this.initVis();
+};
+
+
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+TimePlot.prototype.initVis = function(){
+    var vis = this;
+
+    vis.margin = { top: 40, right: 60, bottom: 60, left: 100 };
+
+    vis.width = 800 - vis.margin.left - vis.margin.right;
+    vis.height = 400 - vis.margin.top - vis.margin.bottom;
+
+
+    // SVG drawing area
+    vis.svg = d3.select("#" + vis.parentElement).append("svg")
+        .attr("width", vis.width + vis.margin.left + vis.margin.right)
+        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+    // Add scales
+    vis.x = d3.scaleTime()
+        .range([0, vis.width]);
+    vis.y = d3.scaleLinear()
+        .range([vis.height, 0]);
+
+    // Add axes
+    vis.xAxis = d3.axisBottom()
+        .scale(vis.x);
+    vis.svg.append('g')
+        .attr('class', 'axis x-axis')
+        .attr("transform", "translate(0," + vis.height + ")");
+    vis.yAxis = d3.axisLeft()
+        .scale(vis.y);
+    vis.svg.append('g')
+        .attr('class', 'axis y-axis');
+
+    // Add y-label
+    vis.svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - vis.margin.left / 3 * 2)
+        .attr("x",0 - (vis.height / 2))
+        .attr("dy", "1em")
+        .attr('class', 'y-label')
+        .style("text-anchor", "middle")
+        .text('Number of People');
+
+    // Create line chart
+    vis.svg.append('path')
+        .attr('class', 'path-line')
+        .attr('id', 'line-killed')
+        .attr('fill', 'none')
+        .attr('stroke', 'black');
+    vis.svg.append('path')
+        .attr('class', 'path-line')
+        .attr('id', 'line-injured')
+        .attr('fill', 'none')
+        .attr('stroke', 'blue');
+
+    // Add legend
+    vis.svg.append('circle')
+        .attr('cx', 100)
+        .attr('cy', 20)
+        .attr('r', 4)
+        .attr('fill', 'black');
+    vis.svg.append('circle')
+        .attr('cx', 100)
+        .attr('cy', 40)
+        .attr('r', 4)
+        .attr('fill', 'blue');
+    vis.svg.append('text')
+        .attr('x', 110)
+        .attr('y', 20)
+        .attr('class', 'legend-text')
+        .text('Killed');
+    vis.svg.append('text')
+        .attr('x', 110)
+        .attr('y', 40)
+        .attr('class', 'legend-text')
+        .text('Injured');
+
+    console.log(vis.allData);
+
+    vis.wrangleData();
+
+};
+
+/*
+ * Data wrangling
+ */
+
+TimePlot.prototype.wrangleData = function(){
+    var vis = this;
+
+    vis.displayData = d3.nest()
+        .key(function(d){ return d3.timeMonth(d.date); })
+        .rollup(function(d) {
+            return {
+                n_killed: d3.sum(d, function(g) {return g.n_killed; }),
+                n_injured: d3.sum(d, function(g) {return g.n_injured})
+            }
+        })
+        .entries(vis.allData);
+    vis.displayData.forEach(function(d){
+        d.key = new Date(d.key);
+    });
+
+    // Set domain of axes
+    vis.x.domain([
+        d3.min(vis.displayData, function(d) {return d.key}),
+        d3.max(vis.displayData, function(d) {return d.key})
+    ]);
+    vis.y.domain([
+        d3.min(vis.displayData, function(d) {return d.value.n_killed}),
+        d3.max(vis.displayData, function(d) {return d.value.n_injured})
+    ]);
+
+    console.log([
+        vis.x(d3.min(vis.displayData, function(d) {return d.key})),
+        vis.x(d3.max(vis.displayData, function(d) {return d.key}))
+    ]);
+
+
+    // Update the visualization
+    vis.updateVis();
+};
+
+
+
+/*
+ * The drawing function - should use the D3 update sequence (enter, update, exit)
+ */
+
+TimePlot.prototype.updateVis = function(){
+    var vis = this;
+
+    // Draw circles for killed
+    var circlesKilled = vis.svg.selectAll('.circle-killed')
+        .data(vis.displayData);
+    circlesKilled.enter()
+        .append('circle')
+        .attr('class', 'circle-killed')
+        .merge(circlesKilled)
+        .attr('cx', function(d) {return vis.x(d.key)})
+        .attr('cy', function(d) {return vis.y(d.value.n_killed)})
+        .attr('r', 4);
+
+    // Draw circles for injured
+    var circlesInjured = vis.svg.selectAll('.circle-injured')
+        .data(vis.displayData);
+    circlesInjured.enter()
+        .append('circle')
+        .attr('class', 'circle-injured')
+        .merge(circlesInjured)
+        .attr('cx', function(d) {return vis.x(d.key)})
+        .attr('cy', function(d) {return vis.y(d.value.n_injured)})
+        .attr('r', 4)
+        .attr('fill', 'blue');
+
+    // Draw axes
+    vis.svg.select('.x-axis')
+        .call(vis.xAxis
+            .ticks(d3.timeMonth.every(3))
+            .tickFormat(d3.timeFormat("%b %Y")))
+        .selectAll('text')
+        .attr("y", 0)
+        .attr("x", -50)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-65)")
+        .style("text-anchor", "start");
+    vis.svg.select('.y-axis')
+        .call(vis.yAxis);
+
+    // Draw line for killed
+    var lineKilled = d3.line()
+        .x(function(d) { return vis.x(d.key); })
+        .y(function(d) { return vis.y(d.value.n_killed); })
+        .curve(d3.curveLinear);
+    var pathKilled = vis.svg
+        .select('#line-killed')
+        .attr('d', lineKilled(vis.displayData));
+
+    // Draw line for injured
+    var lineInjured = d3.line()
+        .x(function(d) { return vis.x(d.key); })
+        .y(function(d) { return vis.y(d.value.n_injured); })
+        .curve(d3.curveLinear);
+    var lineInjured = vis.svg
+        .select('#line-injured')
+        .attr('d', lineInjured(vis.displayData));
+
+};
