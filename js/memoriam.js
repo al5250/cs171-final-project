ce1983636753680f6bc2f038889bcc5dd3f40584
Michel Li <michelli@Michels-MacBook-Pro.local>
2018-11-25 14:16:19 -0500

    working bubble chart

--- js/memoriam.js
+++ js/memoriam.js
@@ -9,7 +9,8 @@
 Memoriam = function(_parentElement, _data){
     this.parentElement = _parentElement;
     this.allData = _data;
-    this.displayData = []; // see data wrangling
+    this.displayData = [];// see data wrangling
+    this.testData = {};
 
     this.initVis();
 };
@@ -25,16 +26,17 @@ Memoriam.prototype.initVis = function(){
 
     vis.margin = { top: 0, right: 100, bottom: 60, left: 0 };
 
-    vis.width = 1000 - vis.margin.left - vis.margin.right;
-    vis.height = 700 - vis.margin.top - vis.margin.bottom;
+    vis.diameter = 550;
 
 
     // SVG drawing area
     vis.svg = d3.select("#" + vis.parentElement).append("svg")
-        .attr("width", vis.width + vis.margin.left + vis.margin.right)
-        .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+        .attr("width", vis.diameter)
+        .attr("height", vis.diameter)
+        .attr("class", "bubble")
         .append("g")
-        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+        .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")")
+        .attr("class", "bubble");
 
     this.allData.forEach(function(d, i) {
         if (d.n_killed > 0) {
@@ -49,7 +51,14 @@ Memoriam.prototype.initVis = function(){
             }
         }
     });
+
+    vis.displayData = vis.displayData.filter(function(d) {
+        return d.age > 0;
+    });
+    console.log(d3.max(vis.displayData, function(d) { return d.age; }));
     console.log(vis.displayData);
+
+    vis.wrangleData();
 };
 
 /*
@@ -58,6 +67,7 @@ Memoriam.prototype.initVis = function(){
 
 Memoriam.prototype.wrangleData = function(){
     var vis = this;
+
 
     // Update the visualization
     vis.updateVis();
@@ -71,6 +81,47 @@ Memoriam.prototype.wrangleData = function(){
 
 Memoriam.prototype.updateVis = function(){
     var vis = this;
+
+
+    vis.testData["children"] = vis.displayData;
+
+    console.log(vis.testData);
+    //max size of the bubbles
+
+    vis.bubble = d3.pack(vis.testData)
+        .size([vis.diameter, vis.diameter])
+        .padding(1.5);
+
+        //bubbles needs very specific format, convert data to this.
+
+    var root = d3.hierarchy(vis.testData)
+        .count(function(d) { return d.age; });
+
+    console.log(root);
+    console.log(vis.bubble(root).descendants())
+
+    vis.node = vis.svg.selectAll(".node")
+        .data(vis.bubble(root).descendants())
+        .enter().filter(function(d){
+            return  !d.children
+        })
+        .append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+    vis.node.append("circle")
+        .attr("r", function(d) { return d.r; })
+        .attr("fill", function(d) {
+            return "gray";
+            if (d.data.age < 15) {
+                return "red";
+            }
+            return "black";
+        })
+
+
+
+    d3.select(self.frameElement).style("height", vis.diameter + "px");
 
 
 };
